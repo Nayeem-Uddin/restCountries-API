15220e004266d4c055168418239a4ab2420299a7
Nayeem-uddin <nayeemu02@gmail.com>
2021-02-08 21:20:35 +0600

    details-info

--- app.js
+++ app.js
@@ -15,9 +15,30 @@ function displayCountries(countries) {
         const countryInfo = `
             <h3 class='country-details'>${country.name}</h3>
             <p class='capital-details'>${country.capital}</p>
+            <button onclick="displayCountryDetails('${country.name}')">view details</button>
         `;
         countryDiv.innerHTML = countryInfo;
         countriesDiv.appendChild(countryDiv);
     }); 
-    
+ 
+}
+const displayCountryDetails = name =>{
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    //console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data =>{
+            //renderCountryDetail[0];
+            renderCountryInfo(data[0])
+        })
+}
+const renderCountryInfo = country =>{
+    //console.log(country);
+    const countryInfo = document.getElementById('countryInfo');
+    countryInfo.innerHTML = `
+        <h1>${country.name}</h1>
+        <h3>${country.area}</h3>
+        <p>${country.population}</p>
+        <img src="${country.flag}">
+    `;
 }
